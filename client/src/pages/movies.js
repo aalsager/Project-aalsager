5d384ceb8e6b36e0c052f07d5c9c3450fc6490b2
Abdulwahab Alsager <abdulwahabalsager@gmail.com>
2024-04-28 10:38:36 -0600

    Set up the ratings out of 10 on the website.

--- client/src/pages/movies.js
+++ client/src/pages/movies.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-const URL = process.env.REACT_APP_URL
+const URL = process.env.REACT_APP_URL;
 
 export default function Movies() {
     const [loading, setLoading] = useState(true);
@@ -9,31 +9,33 @@ export default function Movies() {
         const fetchData = async () => {
             try {
                 const response = await fetch(URL + '/movies', {
-                   headers: {
-                    'Authorization': localStorage.getItem('auth-token')
-                   } 
+                    headers: {
+                        'Authorization': localStorage.getItem('auth-token')
+                    }
                 });
                 const data = await response.json();
-                console.log(data)
 
-                setMovies(data);
+            
+                const sortedMovies = data.sort((a, b) => b.rating - a.rating);
+
+                setMovies(sortedMovies);
                 setLoading(false);
             } catch (error) {
                 console.error('Failed to fetch movies:', error);
-                setLoading(false); 
+                setLoading(false);
             }
         };
 
         fetchData();
-    }, []); 
+    }, []);
 
     return (
         loading ? <div>Loading...</div> :
         <div className='w-full grid grid-cols-4 gap-8 px-4 py-8'>
             {movies.map(movie => (
-                <div className='border-2 h-[50vh] bg-slate-700 flex flex-col' key={movie.id}> 
-                <div className='flex-grow bg-slate-800 border'>
-                    <img alt='movie-poster' src={movie.imageUrl} className='h-full w-full'></img>
+                <div className='border-2 h-[50vh] bg-slate-700 flex flex-col' key={movie.id}>
+                    <div className='flex-grow bg-slate-800 border'>
+                        <img alt='movie-poster' src={movie.imageUrl} className='h-full w-full'></img>
                     </div>
                     <div className='p-2'>
                         <h4 className='text-lg font-semibold'>
@@ -41,6 +43,9 @@ export default function Movies() {
                         </h4>
                         <div className='text-sm'>
                             {(new Date(movie.releaseDate)).toDateString()}
+                        </div>
+                        <div className='text-sm font-bold'>
+                            Rating: {movie.rating} / 10
                         </div>
                     </div>
                 </div>
